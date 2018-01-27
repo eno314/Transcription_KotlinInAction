574621873199c385cc933c4e0ee8fc911a678e07
Hiroto Kitamura <kirikonknow@gmail.com>
2018-01-27 12:07:02 +0900

    単項演算子の定義

--- src/main/kotlin/seven/Main.kt
+++ src/main/kotlin/seven/Main.kt
@@ -1,15 +1,8 @@
 package seven
 
 fun main(args: Array<String>) {
-    val list = arrayListOf(1, 2)
-    // += はlistを変更する
-    list += 3
-
-    // + は全ての要素を持った新しいリストを返す
-    val newList = list + listOf(4, 5)
-
-    println(list)
-    println(newList)
+    val p = Point(10, 20)
+    println(-p)
 }
 
 operator fun Char.times(count: Int): String {
--- src/main/kotlin/seven/Point.kt
+++ src/main/kotlin/seven/Point.kt
@@ -9,3 +9,9 @@ operator fun Point.plus(other: Point): Point {
 operator fun Point.times(scale: Double): Point {
     return Point((x * scale).toInt(), (y * scale).toInt())
 }
+
+// 単項演算子のマイナス関数は引数を持たない
+operator fun Point.unaryMinus(): Point {
+    // 点の座標の符号を反転して返す
+    return Point(-x, -y)
+}
