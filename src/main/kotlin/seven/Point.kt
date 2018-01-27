db937bc06e8f30409fc9dac90e8217399d6fcecd
Hiroto Kitamura <kirikonknow@gmail.com>
2018-01-27 11:23:57 +0900

    演算子をメンバとして宣言する代わりに、演算子を拡張関数としても定義できる 拡張関数での演算子の定義

--- src/main/kotlin/seven/Point.kt
+++ src/main/kotlin/seven/Point.kt
@@ -1,10 +1,7 @@
 package seven
 
-data class Point(val x: Int, val y: Int) {
+data class Point(val x: Int, val y: Int)
 
-    // plusという名前の演算子関数を定義する
-    operator fun plus(other: Point): Point {
-        // 座標を足し合わせて、新しいPointオブジェクトを返す
-        return Point(x + other.x, y + other.y)
-    }
+operator fun Point.plus(other: Point): Point {
+    return Point(x + other.x, y + other.y)
 }
