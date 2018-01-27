00e28928981a9ef25159a4ed0ff6688d9bf0fbba
Hiroto Kitamura <kirikonknow@gmail.com>
2018-01-27 12:10:42 +0900

    インクリメント演算子の定義

--- src/main/kotlin/seven/Main.kt
+++ src/main/kotlin/seven/Main.kt
@@ -1,10 +1,18 @@
 package seven
 
+import java.math.BigDecimal
+
 fun main(args: Array<String>) {
-    val p = Point(10, 20)
-    println(-p)
+    var bd = BigDecimal.ZERO
+    // 1つめのprintlnが実行された後でインクリメントする
+    println(bd++)
+
+    // 2つめのprintlnが実行される前にインクリメントする
+    println(++bd)
 }
 
 operator fun Char.times(count: Int): String {
     return toString().repeat(count)
 }
+
+operator fun BigDecimal.inc() = this + BigDecimal.ONE
