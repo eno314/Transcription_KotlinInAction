a40e0e62a42744199595a157d012568564025d5e
Hiroto Kitamura <kirikonknow@gmail.com>
2018-01-27 11:34:31 +0900

    Pointをある数値で拡縮できる演算子を実装。異なるオペランド型で演算子を定義する

--- src/main/kotlin/seven/Main.kt
+++ src/main/kotlin/seven/Main.kt
@@ -1,9 +1,6 @@
 package seven
 
 fun main(args: Array<String>) {
-    val p1 = Point(10, 20)
-    val p2 = Point(30, 40)
-
-    // + 記号によってplus関数が呼ばれる
-    println(p1 + p2)
+    val p = Point(10, 20)
+    println(p * 1.5)
 }
--- src/main/kotlin/seven/Point.kt
+++ src/main/kotlin/seven/Point.kt
@@ -5,3 +5,7 @@ data class Point(val x: Int, val y: Int)
 operator fun Point.plus(other: Point): Point {
     return Point(x + other.x, y + other.y)
 }
+
+operator fun Point.times(scale: Double): Point {
+    return Point((x * scale).toInt(), (y * scale).toInt())
+}
