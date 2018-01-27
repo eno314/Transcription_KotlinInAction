f0a0ea5b987b9f00ded10076b532d45ac772ef19
Hiroto Kitamura <kirikonknow@gmail.com>
2018-01-28 00:13:14 +0900

    日付のrangeイテレータの実装。日付を繰り返し処理する

--- src/main/kotlin/seven/Main.kt
+++ src/main/kotlin/seven/Main.kt
@@ -1,10 +1,14 @@
 package seven
 
 import java.math.BigDecimal
+import java.time.LocalDate
 
 fun main(args: Array<String>) {
-    for (c in "abc") {
-        println(c)
+    val newYear = LocalDate.ofYearDay(2017, 1)
+    val daysOff = newYear.minusDays(1)..newYear
+    // 対応するiterator関数が利用できるので、daysOff上で繰り返し処理を行う
+    for (dayOff in daysOff) {
+        println(dayOff)
     }
 }
 
@@ -14,5 +18,17 @@ operator fun Char.times(count: Int): String {
 
 operator fun BigDecimal.inc() = this + BigDecimal.ONE
 
-// このライブラリ関数は、文字列を繰り返し処理できる
-// operator fun CharSequence.iterator(): CharIterator
+operator fun ClosedRange<LocalDate>.iterator(): Iterator<LocalDate> =
+        // このオブジェクトは、LocalDate要素を繰り返すIteratorを実装する
+        object : Iterator<LocalDate> {
+            var current = start
+
+            // 日付に対して使用されるcompareToの規約に注意
+            override fun hasNext() = current <= endInclusive
+
+            // 変更する前に、現在の日付を結果として返す
+            override fun next() = current.apply {
+                // 現在の日付を1日インクリメントする
+                current = plusDays(1)
+            }
+        }
