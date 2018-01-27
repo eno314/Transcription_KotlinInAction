f655d6e12ef59603b2842d2a8a512b231b89f95d
Hiroto Kitamura <kirikonknow@gmail.com>
2018-01-27 11:21:16 +0900

    ２つのPointを加算する操作をサポートする XとYの座標のそれぞれに数値を加算する plus演算子を定義する

---
+++ src/main/kotlin/seven/Main.kt
@@ -0,0 +1,9 @@
+package seven
+
+fun main(args: Array<String>) {
+    val p1 = Point(10, 20)
+    val p2 = Point(30, 40)
+
+    // + 記号によってplus関数が呼ばれる
+    println(p1 + p2)
+}
--- src/main/kotlin/seven/Point.kt
+++ src/main/kotlin/seven/Point.kt
@@ -1,3 +1,10 @@
 package seven
 
-data class Point(val x: Int, val y: Int)
+data class Point(val x: Int, val y: Int) {
+
+    // plusという名前の演算子関数を定義する
+    operator fun plus(other: Point): Point {
+        // 座標を足し合わせて、新しいPointオブジェクトを返す
+        return Point(x + other.x, y + other.y)
+    }
+}
