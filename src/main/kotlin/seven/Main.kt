433df39750aed87d4ad6ec55d83c1392ac22e6d9
Hiroto Kitamura <kirikonknow@gmail.com>
2018-01-27 21:32:06 +0900

    Javaで配列にアクセスするのと同じように、Kotlinでは角括弧でマップ内の要素にアクセスできる

--- src/main/kotlin/seven/Main.kt
+++ src/main/kotlin/seven/Main.kt
@@ -3,7 +3,10 @@ package seven
 import java.math.BigDecimal
 
 fun main(args: Array<String>) {
-    println("abc" < "bac")
+    val map = mapOf("hoge" to "fuga")
+    val key = "hoge"
+    val value = map[key]
+    println(value)
 }
 
 operator fun Char.times(count: Int): String {
