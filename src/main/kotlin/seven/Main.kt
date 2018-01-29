5e6812314ccdca53ce796a1e7f34b3917dbcbde2
Hiroto Kitamura <kirikonknow@gmail.com>
2018-01-29 22:56:57 +0900

    【委譲プロパティを使わずに、プロパティに変更があったときにリスナに通知する】 プロパティ変更通知を手動で実装する。年齢と給料が変化したときにリスナに通知する

--- src/main/kotlin/seven/Main.kt
+++ src/main/kotlin/seven/Main.kt
@@ -1,13 +1,19 @@
 package seven
 
+import java.beans.PropertyChangeListener
 import java.math.BigDecimal
 import java.time.LocalDate
 
 fun main(args: Array<String>) {
-    val p = Person("Alice")
-    // emailsは初回アクセス時にロードされる
-    p.emails
-    p.emails
+    val p = Person("Dmitry", 34, 2000)
+    p.addPropertyChangeListener(
+            PropertyChangeListener { event ->
+                println("Property ${event.propertyName} changed " +
+                        "from ${event.oldValue} to ${event.newValue}")
+            })
+
+    p.age = 35
+    p.salary = 2100
 }
 
 operator fun Char.times(count: Int): String {
--- src/main/kotlin/seven/Person.kt
+++ src/main/kotlin/seven/Person.kt
@@ -1,5 +1,20 @@
 package seven
 
-class Person(val name: String) {
-    val emails by lazy { loadEmails(this) }
+class Person(val name: String, age: Int, salary: Int) : PropertyChangeAware() {
+
+    var age: Int = age
+        set(newValue) {
+            // 「field」識別子でプロパティのバッキングフィールドにアクセス
+            val oldValue = field
+            field = newValue
+            // プロパティの値の変更についてリスナに通知
+            changeSupport.firePropertyChange("age", oldValue, newValue)
+        }
+
+    var salary: Int = salary
+        set(newValue) {
+            val oldValue = field
+            field = newValue
+            changeSupport.firePropertyChange("salary", oldValue, newValue)
+        }
 }
