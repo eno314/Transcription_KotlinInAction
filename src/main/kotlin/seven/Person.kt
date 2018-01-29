85af444dc4af3b4f119f9920a5a88a5ebdfc85e7
Hiroto Kitamura <kirikonknow@gmail.com>
2018-01-29 22:46:32 +0900

    委譲プロパティを利用して遅延初期化を実装する

--- src/main/kotlin/seven/Person.kt
+++ src/main/kotlin/seven/Person.kt
@@ -1,16 +1,5 @@
 package seven
 
 class Person(val name: String) {
-    // _emailsプロパティにはemailsが委譲するデータを格納する
-    private var _emails: List<Email>? = null
-
-    val emails: List<Email>
-        get() {
-            if (_emails == null) {
-                // アクセス時にデータをロードする
-                _emails = loadEmails(this)
-            }
-            // 既にデータがロードされていたら、それを返す
-            return _emails!!
-        }
+    val emails by lazy { loadEmails(this) }
 }
