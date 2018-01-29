fe21892d4c3a450f349511c0a35aa6ebeefb6e17
Hiroto Kitamura <kirikonknow@gmail.com>
2018-01-30 08:37:14 +0900

    ハードコーディングしたtoString変換によるjoinToString

--- src/main/kotlin/eight/Main.kt
+++ src/main/kotlin/eight/Main.kt
@@ -5,6 +5,23 @@ fun main(args: Array<String>) {
     println("ab1c".filter { it in 'a'..'z' })
 }
 
+fun <T> Collection<T>.joinToString(
+        separator: String = ", ",
+        prefix: String = "",
+        postfix: String = ""
+): String {
+    val result = StringBuilder(prefix)
+    for ((index, element) in this.withIndex()) {
+        if (index > 0) {
+            result.append(separator)
+        }
+        // デフォルトのtoStringメソッドを使ってオブジェクトをStringへ変換
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
+
 fun String.filter(predicate: (Char) -> Boolean): String {
     val sb = StringBuilder()
     for (index in 0 until length) {
