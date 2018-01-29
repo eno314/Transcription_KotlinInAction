8931635c42c78d80e1710a4047557251e3ca535e
Hiroto Kitamura <kirikonknow@gmail.com>
2018-01-29 23:30:21 +0900

    それぞれのエンティティの属性は、カラムオブジェクトを委譲プロパティとして使用した委譲プロパティとして実装されている

--- src/main/kotlin/seven/User.kt
+++ src/main/kotlin/seven/User.kt
@@ -1,17 +1,15 @@
 package seven
 
 /*
-// オブジェクトはテーブルに対応
 object Users : IdTable() {
-    // プロパティはテーブルのカラムに対応
     val name = varchar("name", lenght = 50).index()
     val age = integer("age")
 }
 
 // Userのそれぞれのインスタンスはテーブルの特定のエントリに対応
 class User(id: EntityID) : Entity(id) {
-    // nameの値はデータベースに格納されているユーザーに対する値
-    var name: Strin by Users.name
+    // Users.nameは、nameプロパティの委譲オブジェクト
+    var name: String by Users.name
     var age: Int by Users.age
 }
 */
