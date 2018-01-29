7f200a4a1c05c3a708e7602b092afc304d59a18f
Hiroto Kitamura <kirikonknow@gmail.com>
2018-01-30 08:12:39 +0900

    関数型の引数の名前を指定することができる 引数名は型マッチングに影響しない。ラムダを宣言するとき、関数型の宣言で使用された引数名と同じものを使用する必要はない。 コードの可読性を向上させ、IDEの補完に使用できる

--- src/main/kotlin/eight/Main.kt
+++ src/main/kotlin/eight/Main.kt
@@ -1,12 +1,21 @@
 package eight
 
 fun main(args: Array<String>) {
-    // Intの型の引数を2つとり、Intの型を返す関数
-    val sum: (Int, Int) -> Int = { x, y -> x + y }
-    // 引数を取らず、戻り値もない関数
-    val action: () -> Unit = { println(42) }
+    val url = "http://kotl.in"
+    // APIで提供されている名前をラムダの引数名として使用可能
+    performRequest(url) { code, content ->
+        /* ... */
+    }
+    // また、それらは変更可能
+    performRequest(url) { code, page ->
+        /* ... */
+    }
+}
 
-    var canReturnNull: (Int, Int) -> Int? = { x, y -> null }
-
-    var funOrNull: ((Int, Int) -> Int)? = null
+fun performRequest(
+        url: String,
+        // 関数型は名前付きの引数を持っている
+        callback: (code: Int, content: String) -> Unit
+) {
+    /*...*/
 }
