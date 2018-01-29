d7e12587916e42659a55827e6d1b50d22a098483
Hiroto Kitamura <kirikonknow@gmail.com>
2018-01-30 08:06:00 +0900

    明示的な型宣言をして、ラムダを変数に格納する 戻り値の型のUnitは、関数型の宣言時には省略できない（常に明示的な戻り値の型が必要なため） 引数x,yの型は関数型の中で定義されているので、ラムダの中では省略できる

--- src/main/kotlin/eight/Main.kt
+++ src/main/kotlin/eight/Main.kt
@@ -1,6 +1,8 @@
 package eight
 
 fun main(args: Array<String>) {
-    val sum = { x: Int, y: Int -> x + y }
-    val action = { println(42) }
+    // Intの型の引数を2つとり、Intの型を返す関数
+    val sum: (Int, Int) -> Int = { x, y -> x + y }
+    // 引数を取らず、戻り値もない関数
+    val action: () -> Unit = { println(42) }
 }
