9649fee47f874b0ea4632181df4d68fcb42d5a31
Hiroto Kitamura <kirikonknow@gmail.com>
2018-01-29 23:09:00 +0900

    ObservalblePropertyのメソッドシグネチャをKotlinの規約が要求する形に変更する  - 委譲プロパティとしてのObservableProperty byキーワードを介して、手動でやっていたことを自動的に行う  - プロパティ変更の通知に委譲プロパティを使用する

--- src/main/kotlin/seven/ObservableProperty.kt
+++ src/main/kotlin/seven/ObservableProperty.kt
@@ -1,16 +1,17 @@
 package seven
 
 import java.beans.PropertyChangeSupport
+import kotlin.reflect.KProperty
 
 class ObservableProperty(
-        val propName: String,
         var propValue: Int,
         val changeSupport: PropertyChangeSupport
 ) {
-    fun getValue(): Int = propValue
-    fun setValue(newValue: Int) {
+    operator fun getValue(p: Person, prop: KProperty<*>): Int = propValue
+
+    operator fun setValue(p: Person, prop: KProperty<*>, newValue: Int) {
         val oldValue = propValue
         propValue = newValue
-        changeSupport.firePropertyChange(propName, oldValue, newValue)
+        changeSupport.firePropertyChange(prop.name, oldValue, newValue)
     }
 }
--- src/main/kotlin/seven/Person.kt
+++ src/main/kotlin/seven/Person.kt
@@ -2,17 +2,7 @@ package seven
 
 class Person(val name: String, age: Int, salary: Int) : PropertyChangeAware() {
 
-    val _age = ObservableProperty("age", age, changeSupport)
-    var age: Int
-        get() = _age.getValue()
-        set(value) {
-            _age.setValue(value)
-        }
+    var age: Int by ObservableProperty(age, changeSupport)
 
-    val _salary = ObservableProperty("salary", salary, changeSupport)
-    var salary: Int
-        get() = _salary.getValue()
-        set(value) {
-            _salary.setValue(value)
-        }
+    var salary: Int by ObservableProperty(salary, changeSupport)
 }
