337957c4559ab4cebb704b93cb62ec84f316b1fd
Hiroto Kitamura <kirikonknow@gmail.com>
2018-01-30 08:07:38 +0900

    関数型の戻り値の型はnull許容型にすることができる。

--- src/main/kotlin/eight/Main.kt
+++ src/main/kotlin/eight/Main.kt
@@ -5,4 +5,6 @@ fun main(args: Array<String>) {
     val sum: (Int, Int) -> Int = { x, y -> x + y }
     // 引数を取らず、戻り値もない関数
     val action: () -> Unit = { println(42) }
+
+    var canReturnNull: (Int, Int) -> Int? = { x, y -> null }
 }
