d9bfa00fcae83d809202c45fb02f138331a58cd5
Hiroto Kitamura <kirikonknow@gmail.com>
2018-01-31 08:31:12 +0900

    引数として2つ以上のラムダとする関数がある場合、そのうちのいくつかだけをインライン化することが可能。 非インラインラムダを受け入れる引数には、noinline修飾子をマークする

--- src/main/kotlin/eight/Main.kt
+++ src/main/kotlin/eight/Main.kt
@@ -9,6 +9,10 @@ fun main(args: Array<String>) {
     }
 }
 
+inline fun foo(inlined: () -> Unit, noinline notInlined: () -> Unit) {
+    // ...
+}
+
 /*
 fun <T, R> Sequence<T>.map(transform: (T) -> R): Sequence<R> {
     return TransformingSequence(this, transform)
