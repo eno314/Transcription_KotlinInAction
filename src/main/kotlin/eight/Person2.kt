d77d65ec5bec8108f8e2dcbc32dc29576063a796
Hiroto Kitamura <kirikonknow@gmail.com>
2018-02-01 08:24:33 +0900

    関数名をreturnのラベルとして使用する ラムダ式のラベルを明示的に指定すると、関数名を使ったラベル付けは機能しない。 ラムダ式は複数のラベルを持つことはできない

--- src/main/kotlin/eight/Person2.kt
+++ src/main/kotlin/eight/Person2.kt
@@ -5,11 +5,10 @@ data class Person2(val name: String, val age: Int)
 val people = listOf(Person2("Alice", 29), Person2("Bob", 31))
 
 fun lookForAlice(people: List<Person2>) {
-    // ラムダ式にラベルを付ける
-    people.forEach label@ {
+    people.forEach {
         if (it.name == "Alice") {
-            // return@labelはこのラベルを参照する
-            return@label
+            // return@forEachはラムダ式からリターン
+            return@forEach
         }
     }
     // この行は常に出力される
