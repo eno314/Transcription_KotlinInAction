2bcf5e14a0dfa3c75c7d295d65bd127a649eacba
Hiroto Kitamura <kirikonknow@gmail.com>
2018-02-01 08:19:09 +0900

    forEachへ渡したラムダ内でreturnを使用する forEach関数をforの代わりに利用しても同じ意味を持つ

--- src/main/kotlin/eight/Person2.kt
+++ src/main/kotlin/eight/Person2.kt
@@ -5,12 +5,12 @@ data class Person2(val name: String, val age: Int)
 val people = listOf(Person2("Alice", 29), Person2("Bob", 31))
 
 fun lookForAlice(people: List<Person2>) {
-    for (person in people) {
-        if (person.name == "Alice") {
+    people.forEach {
+        if (it.name == "Alice") {
             println("Found!")
+            // for文と同じように関数からreturn
             return
         }
     }
-    // リストのpeopleの中にApliceが居なければ、この行が出力される
     println("Alice is not found")
 }
