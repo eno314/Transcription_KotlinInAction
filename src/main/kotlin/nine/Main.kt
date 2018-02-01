b2e04ec83adcaa2b6597e3484b4a1a6729eb8632
Hiroto Kitamura <kirikonknow@gmail.com>
2018-02-02 07:43:02 +0900

    slice関数の宣言例、ジェネリック関数sliceは型パラメータTを持つ 指定された範囲のインデックスに相当する要素のみを含んだリストを返す

--- src/main/kotlin/nine/Main.kt
+++ src/main/kotlin/nine/Main.kt
@@ -4,3 +4,10 @@ fun main(args: Array<String>) {
     val readers: MutableList<String> = mutableListOf()
     val resders2 = mutableListOf<String>()
 }
+
+// 先頭の<T>は型パラメータの宣言
+// ここでは型パラメータはレシーバと戻り値の型で使用される
+fun <T> List<T>.slice(indices: IntRange): List<T> {
+    // ...
+    return listOf()
+}
