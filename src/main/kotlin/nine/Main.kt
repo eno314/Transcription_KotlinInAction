294fd169149c0956504f750ab458c8a528e01c9b
Hiroto Kitamura <kirikonknow@gmail.com>
2018-02-02 08:23:04 +0900

    型パラメータの後に上限境界を指定することで制約を定義。 ジェネリック型の型パラメータに対して上限境界制約として型を指定するとき、ジェネリック型の特定のインスタンス化において対応する型は、指定された型であるか、そのサブタイプでなければならない

--- src/main/kotlin/nine/Main.kt
+++ src/main/kotlin/nine/Main.kt
@@ -4,6 +4,10 @@ fun main(args: Array<String>) {
     println(listOf(1, 2, 3, 4).penultimate)
 }
 
+// Tは型パラメータ
+// : Numberは上限境界
+// fun <T : Number> List<T>.sum(): T
+
 // このジェネリック拡張プロパティは任意のリストで呼び出し可能
 val <T> List<T>.penultimate: T
     get() = this[size - 2]
