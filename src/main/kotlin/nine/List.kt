0ed7131141196918eb2e3f1c1c2932a5870ee21c
Hiroto Kitamura <kirikonknow@gmail.com>
2018-02-02 08:12:05 +0900

    JavaインタフェースListをKotlinではどのように宣言することが出来るのかの例

---
+++ src/main/kotlin/nine/List.kt
@@ -0,0 +1,11 @@
+package nine
+
+/*
+// Listインタフェースは型パラメータTを定義する
+interface List<T> {
+    // インタフェースやクラスの中で、Tは通常の型として使用される
+    operator fun get(index: Int): T {
+        //...
+    }
+}
+*/
--- src/main/kotlin/nine/Main.kt
+++ src/main/kotlin/nine/Main.kt
@@ -4,11 +4,6 @@ fun main(args: Array<String>) {
     println(listOf(1, 2, 3, 4).penultimate)
 }
 
-class Hoge {
-    val <T> x: T = TODO()
-    // Error: Type parameter of a property must be used in its receiver type
-}
-
 // このジェネリック拡張プロパティは任意のリストで呼び出し可能
 val <T> List<T>.penultimate: T
     get() = this[size - 2]
