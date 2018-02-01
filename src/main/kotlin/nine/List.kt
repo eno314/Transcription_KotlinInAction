d5901f6913699051f232afc4e78bfc9cdc511f86
Hiroto Kitamura <kirikonknow@gmail.com>
2018-02-02 08:16:20 +0900

    クラスがジェネリッククラスを継承している場合、基本の型のジェネリックパラメータに型引数を提供する必要がある その場合、特定の型か別の型パラメータになる

--- src/main/kotlin/nine/List.kt
+++ src/main/kotlin/nine/List.kt
@@ -8,4 +8,15 @@ interface List<T> {
         //...
     }
 }
+
+// このクラスはListを実装し、特定の型引数（String）を提供している
+class StringList : List<String> {
+    // Tの代わりにStringを使用する方法に注意
+    override fun get(index: Int): String = ...
+}
+
+// ArrayListのジェネリック型パラメータTは、Listの型引数である
+class ArrayList<T> : List<T> {
+    override fun get(index: Int): T = ...
+}
 */
