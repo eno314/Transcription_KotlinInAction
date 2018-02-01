bb5207d1131d43e29c08810f0003e7d35a4d832a
Hiroto Kitamura <kirikonknow@gmail.com>
2018-02-02 07:50:38 +0900

    ジェネリック高階関数の呼び出し この場合、ラムダ式の引数itの型はString。なぜなら、この関数宣言の中で、ラムダ式の引数はジェネリック型Tを持つとコンパイラが推論することになるから。

--- src/main/kotlin/nine/Main.kt
+++ src/main/kotlin/nine/Main.kt
@@ -1,9 +1,10 @@
 package nine
 
 fun main(args: Array<String>) {
-    val letters = ('a'..'z').toList()
-    // 型引数の明示的な指定
-    println(letters.slice<Char>(0..2))
-    // 「TはCharである」とコンパイラが推論する
-    println(letters.slice(10..13))
+    val authors = listOf("Dmitry", "Svetlana")
+    val readers = mutableListOf<String>(/*...*/)
+
+    readers.filter { it !in authors }
 }
+
+// fun <T> List<T>.filter(predicate: (T) -> Boolean): List<T>
