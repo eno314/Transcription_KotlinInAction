563781d4ef16a7eec0af7b3bb7c3f75ca7d62680
Hiroto Kitamura <kirikonknow@gmail.com>
2018-02-05 01:03:13 +0900

    クラスの型パラメータにおけるoutキーワードによって、Tを使う全てのメソッドは、inポジションにではなくoutポジションのみにTを持つことが義務付けられる。このキーワードは、Tの使用可能性を制限し、対応するサブタイプ関係の安全性を保証する Herdクラスは、getメソッドの戻り値としてだけに、型パラメータTを使用する。 これが、クラスを共変として宣言することを安全にするoutポジションという概念

--- src/main/kotlin/nine/Animal.kt
+++ src/main/kotlin/nine/Animal.kt
@@ -1,6 +1,5 @@
 package nine
 
-/*
 open class Animal {
     fun feed() {
         // ...
@@ -12,11 +11,13 @@ class Herd<out T : Animal> {
     val size: Int
         get() = 0 // ...
 
+    // 戻り値の型としてTを使用
     operator fun get(i: Int): T {
         // ...
     }
 }
 
+/*
 fun feedAll(animals: Herd<Animal>) {
     for (i in 0 until animals.size) {
         animals[i].feed()
