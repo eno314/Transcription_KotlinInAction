3ea3f91b3b859b0ffba9a404ee093ccc9a2f726d
Hiroto Kitamura <kirikonknow@gmail.com>
2018-02-06 08:14:04 +0900

    テストのタイムアウトをミリ秒で指定する アノテーションの引数は、コンパイル時に知る必要があるので、プロパティをアノテーションの引数として使うためには、const修飾子を付ける必要がある。この修飾子は、コンパイラに対して、そのプロパティがコンパイル時定数であることを伝える

--- src/test/kotlin/ten/MyTest.kt
+++ src/test/kotlin/ten/MyTest.kt
@@ -3,9 +3,17 @@ package ten
 import org.junit.Assert
 import org.junit.Test
 
+const val TEST_TIMEOUT = 100L
+
 class MyTest {
     // @Testアノテーションは、JUnitフレームワークにテストとして実行したいメソッドを教える
-    @Test fun testTrue() {
+    @Test
+    fun testTrue() {
+        Assert.assertTrue(true)
+    }
+
+    @Test(timeout = TEST_TIMEOUT)
+    fun testMethod() {
         Assert.assertTrue(true)
     }
 }
