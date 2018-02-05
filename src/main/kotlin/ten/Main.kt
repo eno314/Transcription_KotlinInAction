5ebc0d226fa710885188cf1d346990fcfdb9e44d
Hiroto Kitamura <kirikonknow@gmail.com>
2018-02-06 08:11:01 +0900

    @DeprecatedアノテーションはJavaでの意味と同じだが、replaceWithという引数によって拡張されている。 これを使って、非推奨である旨のメッセージと代替パターンを指定する。 IntellijIDEAでは、どの関数を代わりに使うべきかを示すだけではなく、自動的に置き換えるためのクイックフィックスも提供する

---
+++ src/main/kotlin/ten/Main.kt
@@ -0,0 +1,14 @@
+package ten
+
+fun main(args: Array<String>) {
+    remove(1)
+}
+
+@Deprecated("Use removeAt(index) instead.", ReplaceWith("removeAt(index)"))
+fun remove(index: Int) {
+    // ...
+}
+
+fun removeAt(index: Int) {
+    // ...
+}
