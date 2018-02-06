9bbf834d20fe732a2800935bb1c6999ece7b554a
Hiroto Kitamura <kirikonknow@gmail.com>
2018-02-07 08:48:43 +0900

    invokeメソッドを使ってメソッドを呼び出す。 invokeメソッドは決まった数の引数を受け取り、それらの型はKFunctionインタフェースの型パラメータに対応している kFounctionを直接呼び出すこともできる。 引数の数が間違っていると、コンパイルできない。

--- src/main/kotlin/ten/Main.kt
+++ src/main/kotlin/ten/Main.kt
@@ -1,9 +1,16 @@
 package ten
 
+import kotlin.reflect.KFunction2
+
 fun main(args: Array<String>) {
-    val kFunction = ::foo
-    kFunction.call(42)
+    val kFunction: KFunction2<Int, Int, Int> = ::sum
+    println(kFunction.invoke(1, 2) + kFunction(3, 4))
+
+    // kFunction(1)
+    // ERROR: No value passed for parameter p2
 }
+
+fun sum(x: Int, y: Int) = x + y
 
 fun foo(x: Int) = println(x)
 
