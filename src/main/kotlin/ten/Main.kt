e5a2dc3b3094be1710c0eff6dc4f3277d4fb3f7e
Hiroto Kitamura <kirikonknow@gmail.com>
2018-02-07 08:52:11 +0900

    KPropertyインスタンスにおいてもcallメソッドを呼び出すことができるが、プロパティインタフェースには、プロパティの値を取得するために、getメソッドという更に良い手段が用意されている。 トップレベルのプロパティはKProperty0インタフェースのインスタンスで表され、そのインタフェースは引数なしのgetメソッドを持っている。

--- src/main/kotlin/ten/Main.kt
+++ src/main/kotlin/ten/Main.kt
@@ -1,13 +1,13 @@
 package ten
 
-import kotlin.reflect.KFunction2
+var counter = 0
 
 fun main(args: Array<String>) {
-    val kFunction: KFunction2<Int, Int, Int> = ::sum
-    println(kFunction.invoke(1, 2) + kFunction(3, 4))
-
-    // kFunction(1)
-    // ERROR: No value passed for parameter p2
+    val kProperty = ::counter
+    // リフレクションを使ってsetterを呼び出し、引数として21を渡す
+    kProperty.set(21)
+    // getを呼び出してプロパティの値を取得する
+    println(kProperty.get())
 }
 
 fun sum(x: Int, y: Int) = x + y
