0e8226d20707360bc52cdda9c6f2a642ed4d4476
Hiroto Kitamura <kirikonknow@gmail.com>
2018-02-07 00:05:15 +0900

    Dateをシリアライズする必要があって、ValueSerializer<Date>インタフェースを実装した独自のDateSerializerクラスを作る これをPersonクラスに適用する

--- src/main/kotlin/ten/Main.kt
+++ src/main/kotlin/ten/Main.kt
@@ -1,6 +1,15 @@
 package ten
 
+import ru.yole.jkid.deserialization.deserialize
+import ru.yole.jkid.serialization.serialize
+import java.text.SimpleDateFormat
+
 fun main(args: Array<String>) {
+    val person = Person("Alice", SimpleDateFormat("dd-mm-yyyy").parse("13-02-1987"))
+    println(serialize(person))
+
+    val json = """{"birthDate": "13-02-1987", "name": "Alice"}"""
+    println(deserialize<Person>(json))
 }
 
 @Deprecated("Use removeAt(index) instead.", ReplaceWith("removeAt(index)"))
--- src/main/kotlin/ten/Person.kt
+++ src/main/kotlin/ten/Person.kt
@@ -1,9 +1,10 @@
 package ten
 
-import ru.yole.jkid.DeserializeInterface
+import ru.yole.jkid.CustomSerializer
+import java.util.*
 
 data class Person(
         val name: String,
-        @DeserializeInterface(Company::class)
-        val company: Company
+        @CustomSerializer(DateSerializer::class)
+        val birthDate: Date
 )
