d7c7b93bfa1f84b9992f02cdf496cfa1159a0404
Hiroto Kitamura <kirikonknow@gmail.com>
2018-02-06 23:48:00 +0900

    独自のメタアノテーションを宣言するには、対象としてANNOTATION_CLASSを使う

--- src/main/kotlin/ten/Annotations.kt
+++ src/main/kotlin/ten/Annotations.kt
@@ -4,3 +4,9 @@ package ten
 annotation class JsonExclude
 
 annotation class JsonName(val name: String)
+
+@Target(AnnotationTarget.ANNOTATION_CLASS)
+annotation class BindingAnnotation
+
+@BindingAnnotation
+annotation class MyBinding
