57e882886f13298e85db96a861f7427ebd979be5
Hiroto Kitamura <kirikonknow@gmail.com>
2018-02-07 09:08:30 +0900

    オブジェクトのシリアライゼーション クラスのプロパティを順番にシリアライズしている。

--- src/main/kotlin/ten/Main.kt
+++ src/main/kotlin/ten/Main.kt
@@ -1,11 +1,29 @@
 package ten
 
+import ru.yole.jkid.joinToStringBuilder
+import kotlin.reflect.full.memberProperties
+
 var counter = 0
 
 fun main(args: Array<String>) {
     val person = Person("Alice", 29)
     val memberProperty = Person::age
     println(memberProperty.get(person))
+}
+
+private fun StringBuilder.serializeObject(obj: Any) {
+    // オブジェクトに対するKClassを取得
+    val kClass = obj.javaClass.kotlin
+    // クラスの全てのプロパティを取得
+    val properties = kClass.memberProperties
+
+    properties.joinToStringBuilder(this, prefix = "{", postfix = "}") { prop ->
+        // プロパティ名を取得
+        serializeString(prop.name)
+        append(": ")
+        // プロパティの値を取得
+        serializePropertyValue(prop.get(obj))
+    }
 }
 
 fun sum(x: Int, y: Int) = x + y
