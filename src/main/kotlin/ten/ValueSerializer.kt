0a2f039843bd7cea5843b21b2037a39f99d9934d
Hiroto Kitamura <kirikonknow@gmail.com>
2018-02-10 08:00:16 +0900

    値の型に応じてシリアライザを取得する

--- src/main/kotlin/ten/ValueSerializer.kt
+++ src/main/kotlin/ten/ValueSerializer.kt
@@ -1,6 +1,21 @@
 package ten
 
+import ru.yole.jkid.BooleanSerializer
+import ru.yole.jkid.ByteSerializer
+import ru.yole.jkid.IntSerializer
+import ru.yole.jkid.ValueSerializer
+import java.lang.reflect.Type
+
 interface ValueSerializer<T> {
     fun toJsonValue(value: T): Any?
     fun fromJsonValue(jsonValue: Any?): T
 }
+
+fun serializerForType(type: Type): ValueSerializer<out Any?>? =
+        when (type) {
+            Byte::class.java -> ByteSerializer
+            Int::class.java -> IntSerializer
+            Boolean::class.java -> BooleanSerializer
+        // ...
+            else -> null
+        }
