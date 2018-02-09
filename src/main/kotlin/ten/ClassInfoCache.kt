5e7aa575989273dac920692f89e61f32095c18ac
Hiroto Kitamura <kirikonknow@gmail.com>
2018-02-10 08:08:16 +0900

    キャッシュされたリフレクションデータの保持

---
+++ src/main/kotlin/ten/ClassInfoCache.kt
@@ -0,0 +1,12 @@
+package ten
+
+import ru.yole.jkid.deserialization.ClassInfo
+
+class ClassInfoCache {
+    private val cacheData = mutableMapOf<KClass<*>, ClassInfo<*>>()
+
+    @Suppress("UNCHECKED_CAST")
+    operator fun <T: Any> get(cls: KClass<T>): ClassInfo<T> = cacheData.getOrPut(cls) {
+        ClassInfo(cls)
+    } as ClassInfo<T>
+}
